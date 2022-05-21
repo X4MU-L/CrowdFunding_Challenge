e5edaadaa985f8427c3c5e4768f33fd1f8073037
Okoli Samuel <okolisamuel21@gmail.com>
2022-05-21 12:03:50 +0100

    [added the CTA sections] i have added the hero section of this design. and also updated the necessary form sections

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ selectElements(".menu-btn").addEventListener("click", () =>{
 selectElements(".backdrop").addEventListener("click", () =>{
     selectElements(".nav-links-right-container").parentElement.classList.remove("activated");
 })
+
+
+//This code is incomplete as i want to finish the modals before continuing
+const dollar = "$"
+const amountBacked = 89914
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+selectElements(".amount-backed").textContent = `${dollar}${numberWithCommas(amountBacked)}`
+
